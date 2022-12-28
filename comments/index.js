c24361a33ffa1ae28cb1007d3f70f75215c29a02
MarceloArraes <marcelo.arraes@gmail.com>
2022-12-28 14:44:19 +0000

    feat: comments and post crud working and react app also updated to 18.2.0

---
+++ comments/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  console.log('get posts');
+  res.send(commentsByPostId[req.params.id] || [])
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  console.log('post(/posts/:id/comments');
+  const commentId = randomBytes(4).toString('hex');
+
+  const { content } = req.body
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({id: commentId, content})
+  commentsByPostId[req.params.id] = comments;
+  console.log('so far so.. ', comments, content);
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('listening 4001...');
+})
